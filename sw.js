6850531fd9d51b75c5b39f95c430d27c16ddc057
sshashmicpu <videowalidiary@gmail.com>
2026-01-25 00:32:34 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@ const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
   '/manifest.json',
-  '68961.png',
+  'icon.png',
   'https://fonts.googleapis.com/css2?family=Amiri:ital,wght@0,400;0,700;1,400&family=Noto+Nastaliq+Urdu&family=Inter:wght@400;600&display=swap'
 ];
 
